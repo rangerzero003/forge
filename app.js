13c0cb430f37840fbaac8158c90693d2a7bc8e04
Ben Veen <benjamincveen@gmail.com>
2018-07-03 15:41:52 -0700

    async await example

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const Express = require('express');
+const request = require('bluebird').promisify(require('request'));
 
 const app = Express();
 const port = process.env.PORT || 80;
+
+async function retrievePokemon(id) {
+    console.log(id);
+    let pokemon = await request('https://pokeapi.co/api/v2/pokemon/' + id);
+    console.log(pokemon.body);
+    return pokemon;
+}
 
 app.route('/')
 .get((request, response, next) => {
     response.send('Forge is up and running on Azure');
 });
 
-app.listen(port, () => {
-    console.log('App started and listening on ' + port);
-});
+app.route('/pokemon/:id')
+.get(async (request, response, next) => {
+    response.send(JSON.stringify(await retrievePokemon(request.params.id)));
+})
+
+app.listen(3000);
